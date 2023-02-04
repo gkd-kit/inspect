fa02a59744ccb753c58386685f06b21ad7c8f57b
lisonge <i@songe.li>
2023-02-04 15:59:31 +0800

    fix: permissions

---
+++ src/views/HomePage.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+<template>
+  <div class="HomePage"></div>
+</template>
+<style lang="scss" scoped>
+.HomePage {
+  display: flex;
+  flex-direction: column;
+}
+</style>
