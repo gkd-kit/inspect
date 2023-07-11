dfb60e37d1342103ef20e290160cd3df1e9290c5
lisonge <i@songe.li>
2023-07-11 20:51:27 +0800

    fix: hide empty commit value

--- src/utils/commit.ts
+++ src/utils/commit.ts
@@ -3,6 +3,7 @@ import latestLog from 'virtual:commit';
 export const commitLog =
   `GIT commit\n` +
   Object.entries(latestLog)
+    .filter(([_, value]: [string, string]) => String(value || ``).trim())
     .map(([key, value]) => {
       return key + ': ' + value;
     })
