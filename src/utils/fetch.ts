e533cdf030bb87ab3f1ae4355ba2661b863ec67d
lisonge <i@songe.li>
2023-08-25 21:09:50 +0800

    feat: use proxy when import

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -5,14 +5,25 @@ import { isAllowCorsUrl } from './url';
 export const enhanceFetch = async (
   input: RequestInfo | URL,
   init?: RequestInit,
+  options?: { proxy?: boolean },
 ) => {
-  const u = new URL(new Request(input).url);
+  const req = new Request(input);
+  const u = new URL(req.url);
   if (isAllowCorsUrl(u)) return fetch(input, init);
 
   if (gmOk()) {
     // with cookie
     // export snapshot need
     return GM_fetch(input, init);
+  } else if (options?.proxy) {
+    const proxyUrl = new URL(`https://proxy-workers.lisonge.workers.dev/`);
+    proxyUrl.searchParams.set(`proxyUrl`, u.href);
+    const request = new Request(input, init);
+    return fetch(proxyUrl, {
+      method: request.method,
+      headers: request.headers,
+      body: request.body,
+    });
   } else {
     store.networkErrorDlgVisible = true;
     throw new Error(`gm is not supported`);
