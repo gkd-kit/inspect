5588ee0cbe4aaf0173482fdb5a3519fff0d98132
lisonge <i@songe.li>
2024-04-17 12:08:55 +0800

    perf: pkg version

--- scripts/updateVersion.ts
+++ scripts/updateVersion.ts
@@ -1,11 +1,12 @@
 import fs from 'node:fs/promises';
 import type selfPkgT from '../package.json';
+import process from 'node:process';
 
 const selfPkg: typeof selfPkgT = JSON.parse(
   await fs.readFile(process.cwd() + '/package.json', 'utf-8'),
 );
-
-selfPkg.version = `0.0.` + Date.now();
+selfPkg.version =
+  selfPkg.version.split('.').slice(0, 2).join('.') + '.' + Date.now();
 
 await fs.writeFile(
   process.cwd() + '/package.json',
