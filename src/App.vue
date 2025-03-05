7f952301cafacaa6866c332f6dc7cd311c58255a
二刺螈 <i@songe.li>
2025-03-05 18:05:07 +0800

    perf: auto free active button

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,22 @@ import { dateZhCN, zhCN, type GlobalThemeOverrides } from 'naive-ui';
 import { RouterView } from 'vue-router';
 import ErrorDlg from './components/ErrorDlg.vue';
 import { ScrollbarWrapper } from './utils/others';
+import { debounce } from 'lodash-es';
 
 const themeOverrides: GlobalThemeOverrides = {
   common: {
     lineHeight: '20px',
   },
 };
+
+const freeActiveElement = debounce(() => {
+  if (document.activeElement instanceof HTMLButtonElement) {
+    document.activeElement.blur();
+  }
+}, 1000);
+useEventListener('click', () => {
+  freeActiveElement();
+});
 </script>
 <template>
   <NConfigProvider
